2d601c13ea8d64b8cdf7142d7253c5a7437fd1f8
Anders Engström <anders.engstrom@leogears.com>
2017-01-21 19:18:38 +0100

    wip: coordinate systems etc
    Not working.

--- src/snake/ui.clj
+++ src/snake/ui.clj
@@ -1,15 +1,31 @@
 (ns snake.ui
   (:require [quil
              [core :as q]
-             [middleware :as m]]))
+             [middleware :as m]]
+            [snake.core :refer :all]))
 
-(defn initialize [])
-(defn draw [state])
-(defn next-state [state])
+(def ^:dynamic *cell-size* 10)
 
-(q/defsketch snake-game
-  :setup #'initialize
-  :draw #'draw
-  :update #'next-state
-  :size [100 100]
-  :middleware [m/fun-mode])
+(defn initialize []
+  (make-state (/ (q/width) *cell-size*)
+              (/ (q/height) *cell-size*)))
+
+(defn draw [state]
+  ;; Set the scale to use.
+  (q/scale  0.2)
+  (q/clear)
+  (q/background 255 255 255)
+  (let [[x y] (:position state)]
+    (q/ellipse ))
+  )
+(defn next-state [state] state)
+
+
+(defn start-sketch []
+  (q/sketch
+   :features [:keep-on-top]
+   :setup #'initialize
+   :draw #'draw
+   :update #'next-state
+   :size [500 500]
+   :middleware [m/fun-mode]))
